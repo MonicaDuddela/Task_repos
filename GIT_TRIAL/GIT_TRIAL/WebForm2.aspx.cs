c69d8cf5434b0deb56679dd47c4f3a5aead42ccf
MonicaDuddela <monica.duddela@smscountry.com>
2019-08-21 04:02:55 -0700

    commit 2

--- GIT_TRIAL/GIT_TRIAL/WebForm2.aspx.cs
+++ GIT_TRIAL/GIT_TRIAL/WebForm2.aspx.cs
@@ -12,6 +12,7 @@ namespace GIT_TRIAL
         protected void Page_Load(object sender, EventArgs e)
         {
             Response.Write("hiii");
+            Response.Write("hellloooooooo");
         }
     }
 }
